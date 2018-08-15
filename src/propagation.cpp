03128a19c0e5fabed05bc7549f326c4d5b71a410
Will Gittoes <will.gittoes@datadoghq.com>
2018-08-15 11:21:26 +1000

    Seperate PRs. Revert "Fix issue where a lack of context was considered an error."
    This reverts commit 44f0e8f6358534bd6c9c62d7e0309a50c87e0786.

--- src/propagation.cpp
+++ src/propagation.cpp
@@ -139,20 +139,15 @@ ot::expected<std::unique_ptr<ot::SpanContext>> SpanContext::deserialize(
         }
         return {};
       });
-  if (!result) {  // "if unexpected", hence "return {}" from above is fine.
+  if (!result) {  // "if unexpected", hence "{}" from above is fine.
     return ot::make_unexpected(result.error());
   }
-  if (!trace_id_set && !parent_id_set) {
-    return {};  // Empty context/no context provided.
-  }
   if (!trace_id_set || !parent_id_set) {
-    // Partial context, this shouldn't happen.
     return ot::make_unexpected(ot::span_context_corrupted_error);
   }
   return std::move(
       std::unique_ptr<ot::SpanContext>{new SpanContext{parent_id, trace_id, std::move(baggage)}});
-}  // namespace opentracing
-catch (const std::bad_alloc &) {
+} catch (const std::bad_alloc &) {
   return ot::make_unexpected(std::make_error_code(std::errc::not_enough_memory));
 }
 
