edb7d866ebdd4c6973bccf2c81354bf1060f4143
Will Gittoes <will.gittoes@datadoghq.com>
2018-08-22 15:29:48 +1000

    Conditionally compile dependencies

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,49 +1,78 @@
 #!/bin/bash
 set -eo pipefail
 
-# OpenTracing
-wget https://github.com/opentracing/opentracing-cpp/archive/v${OPENTRACING_VERSION}.tar.gz -O opentracing-cpp.tar.gz
-tar zxvf opentracing-cpp.tar.gz
-mkdir opentracing-cpp-${OPENTRACING_VERSION}/.build
-cd opentracing-cpp-${OPENTRACING_VERSION}/.build
-cmake -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_CXX_FLAGS="-fPIC" \
-      -DBUILD_SHARED_LIBS=OFF \
-      -DBUILD_TESTING=OFF \
-      -DBUILD_MOCKTRACER=OFF \
-      ..
-make
-make install
-cd ../..
+BUILD_OPENTRACING=1
+BUILD_CURL=1
+BUILD_MSGPACK=1
+
+OPENTRACING_VERSION=${OPENTRACING_VERSION:-1.4.0}
+CURL_VERSION=${CURL_VERSION:-7.60}
+MSGPACK_VERSION=${MSGPACK_VERSION:-3.0.1}
+
+while test $# -gt 0
+do
+  case "$1" in
+    not-msgpack) BUILD_MSGPACK=0
+      ;;
+    not-opentracing) BUILD_OPENTRACING=0
+      ;;
+    not-curl) BUILD_CURL=0
+      ;;
+    *) echo "unknown dependency: $1"
+      ;;
+  esac
+  shift
+done
 
 # Msgpack
-wget https://github.com/msgpack/msgpack-c/releases/download/cpp-${MSGPACK_VERSION}/msgpack-${MSGPACK_VERSION}.tar.gz -O msgpack.tar.gz
-tar zxvf msgpack.tar.gz
-mkdir msgpack-${MSGPACK_VERSION}/.build
-cd msgpack-${MSGPACK_VERSION}/.build
-cmake ..
-make
-make install
-cd ../..
+if [ "$BUILD_MSGPACK" -eq "1" ]; then
+  wget https://github.com/msgpack/msgpack-c/releases/download/cpp-${MSGPACK_VERSION}/msgpack-${MSGPACK_VERSION}.tar.gz -O msgpack.tar.gz
+  tar zxvf msgpack.tar.gz
+  mkdir msgpack-${MSGPACK_VERSION}/.build
+  cd msgpack-${MSGPACK_VERSION}/.build
+  cmake ..
+  make
+  make install
+  cd ../..
+fi
+
+# OpenTracing
+if [ "$BUILD_OPENTRACING" -eq "1" ]; then
+  wget https://github.com/opentracing/opentracing-cpp/archive/v${OPENTRACING_VERSION}.tar.gz -O opentracing-cpp.tar.gz
+  tar zxvf opentracing-cpp.tar.gz
+  mkdir opentracing-cpp-${OPENTRACING_VERSION}/.build
+  cd opentracing-cpp-${OPENTRACING_VERSION}/.build
+  cmake -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_CXX_FLAGS="-fPIC" \
+        -DBUILD_SHARED_LIBS=OFF \
+        -DBUILD_TESTING=OFF \
+        -DBUILD_MOCKTRACER=OFF \
+        ..
+  make
+  make install
+  cd ../..
+fi
 
 # Libcurl
-wget https://curl.haxx.se/download/curl-${CURL_VERSION}.tar.gz
-tar zxf curl-${CURL_VERSION}.tar.gz
-cd curl-${CURL_VERSION}
-./configure --disable-ftp \
-            --disable-ldap \
-            --disable-dict \
-            --disable-telnet \
-            --disable-tftp \
-            --disable-pop3 \
-            --disable-smtp \
-            --disable-gopher \
-            --without-ssl \
-            --disable-crypto-auth \
-            --without-axtls \
-            --disable-rtsp \
-            --enable-shared=no \
-            --enable-static=yes \
-            --with-pic
-make && make install
-cd ..
+if [ "$BUILD_CURL" -eq "1" ]; then
+  wget https://curl.haxx.se/download/curl-${CURL_VERSION}.tar.gz
+  tar zxf curl-${CURL_VERSION}.tar.gz
+  cd curl-${CURL_VERSION}
+  ./configure --disable-ftp \
+              --disable-ldap \
+              --disable-dict \
+              --disable-telnet \
+              --disable-tftp \
+              --disable-pop3 \
+              --disable-smtp \
+              --disable-gopher \
+              --without-ssl \
+              --disable-crypto-auth \
+              --without-axtls \
+              --disable-rtsp \
+              --enable-shared=no \
+              --enable-static=yes \
+              --with-pic
+  make && make install
+  cd ..
+fi
