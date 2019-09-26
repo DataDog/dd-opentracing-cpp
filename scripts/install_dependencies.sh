010c5ac007ef3050c347f35a60f0e8dd40e08bb3
Caleb Gilmour <caleb.gilmour@datadoghq.com>
2019-09-27 09:23:15 +1200

    Build / install updates  (#112)

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,10 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 set -e
+install_dir=$(mkdir -p "${0%/*}/../deps" && cd "${0%/*}/../deps" && echo "$PWD")
+if [[ ! -d "$install_dir" ]]; then
+	echo "Unable to determine install directory"
+	exit 1
+fi
 
 OPENTRACING_VERSION=${OPENTRACING_VERSION:-1.5.1}
-CURL_VERSION=${CURL_VERSION:-7.64.0}
-MSGPACK_VERSION=${MSGPACK_VERSION:-3.1.1}
+CURL_VERSION=${CURL_VERSION:-7.66.0}
+MSGPACK_VERSION=${MSGPACK_VERSION:-3.2.0}
 ZLIB_VERSION=${ZLIB_VERSION:-1.2.11}
+
+# Just report versions and exit.
+if [[ "$1" == "versions" ]]; then
+	echo "opentracing:$OPENTRACING_VERSION"
+	echo "curl:$CURL_VERSION"
+	echo "msgpack:$MSGPACK_VERSION"
+	echo "zlib:$ZLIB_VERSION"
+	exit 0
+fi
 
 # Allow specifying dependencies not to install. By default we want to compile
 # our own versions, but under some circumstances (eg building opentracing-nginx
@@ -33,11 +47,12 @@ done
 
 # OpenTracing
 if [ "$BUILD_OPENTRACING" -eq "1" ]; then
-  wget https://github.com/opentracing/opentracing-cpp/archive/v${OPENTRACING_VERSION}.tar.gz -O opentracing-cpp.tar.gz
-  tar zxvf opentracing-cpp.tar.gz
-  mkdir opentracing-cpp-${OPENTRACING_VERSION}/.build
-  cd opentracing-cpp-${OPENTRACING_VERSION}/.build
-  cmake -DCMAKE_BUILD_TYPE=Release \
+  wget "https://github.com/opentracing/opentracing-cpp/archive/v${OPENTRACING_VERSION}.tar.gz" -O opentracing-cpp.tar.gz
+  tar zxf opentracing-cpp.tar.gz
+  mkdir -p "opentracing-cpp-${OPENTRACING_VERSION}/.build"
+  cd "opentracing-cpp-${OPENTRACING_VERSION}/.build"
+  cmake -DCMAKE_INSTALL_PREFIX="$install_dir" \
+        -DCMAKE_BUILD_TYPE=Release \
         -DCMAKE_CXX_FLAGS="-fPIC" \
         -DBUILD_SHARED_LIBS=OFF \
         -DBUILD_TESTING=OFF \
@@ -46,36 +61,45 @@ if [ "$BUILD_OPENTRACING" -eq "1" ]; then
   make
   make install
   cd ../..
+  rm -r "opentracing-cpp-${OPENTRACING_VERSION}/"
+  rm opentracing-cpp.tar.gz
 fi
 
 # Zlib
 if [ "$BUILD_ZLIB" -eq "1" ]; then
-  wget https://zlib.net/zlib-${ZLIB_VERSION}.tar.gz
-  tar zxf zlib-${ZLIB_VERSION}.tar.gz
-  cd zlib-${ZLIB_VERSION}
-  CFLAGS="$CFLAGS -fPIC" ./configure --static
+  wget "https://zlib.net/zlib-${ZLIB_VERSION}.tar.gz"
+  tar zxf "zlib-${ZLIB_VERSION}.tar.gz"
+  mkdir -p "zlib-${ZLIB_VERSION}"
+  cd "zlib-${ZLIB_VERSION}"
+  CFLAGS="$CFLAGS -fPIC" ./configure --prefix="$install_dir" --static
   make && make install
   cd ..
+  rm -r "zlib-${ZLIB_VERSION}"
+  rm "zlib-${ZLIB_VERSION}.tar.gz"
 fi
 
 # Msgpack
 if [ "$BUILD_MSGPACK" -eq "1" ]; then
-  wget https://github.com/msgpack/msgpack-c/releases/download/cpp-${MSGPACK_VERSION}/msgpack-${MSGPACK_VERSION}.tar.gz -O msgpack.tar.gz
-  tar zxvf msgpack.tar.gz
-  mkdir msgpack-${MSGPACK_VERSION}/.build
-  cd msgpack-${MSGPACK_VERSION}/.build
-  cmake -DBUILD_SHARED_LIBS=OFF ..
+  wget "https://github.com/msgpack/msgpack-c/releases/download/cpp-${MSGPACK_VERSION}/msgpack-${MSGPACK_VERSION}.tar.gz" -O msgpack.tar.gz
+  tar zxf msgpack.tar.gz
+  mkdir -p "msgpack-${MSGPACK_VERSION}/.build"
+  cd "msgpack-${MSGPACK_VERSION}/.build"
+  cmake -DCMAKE_INSTALL_PREFIX="$install_dir" -DBUILD_SHARED_LIBS=OFF ..
   make
   make install
   cd ../..
+  rm -r "msgpack-${MSGPACK_VERSION}/"
+  rm msgpack.tar.gz
 fi
 
 # Libcurl
 if [ "$BUILD_CURL" -eq "1" ]; then
-  wget https://curl.haxx.se/download/curl-${CURL_VERSION}.tar.gz
-  tar zxf curl-${CURL_VERSION}.tar.gz
-  cd curl-${CURL_VERSION}
-  ./configure --disable-ftp \
+  wget "https://curl.haxx.se/download/curl-${CURL_VERSION}.tar.gz"
+  tar zxf "curl-${CURL_VERSION}.tar.gz"
+  mkdir -p "curl-${CURL_VERSION}"
+  cd "curl-${CURL_VERSION}"
+  ./configure --prefix="$install_dir" \
+              --disable-ftp \
               --disable-ldap \
               --disable-dict \
               --disable-telnet \
@@ -93,4 +117,6 @@ if [ "$BUILD_CURL" -eq "1" ]; then
               --with-pic
   make && make install
   cd ..
+  rm -r "curl-${CURL_VERSION}/"
+  rm "curl-${CURL_VERSION}.tar.gz"
 fi
