7ada833bf3bd24d27b3baa330078262071d455e6
David Goffredo <dmgoffredo@gmail.com>
2021-06-12 16:11:03 -0400

    add code formatter consistent with the CI check

---
+++ scripts/format.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+usage() {
+    argv0="$0"
+    
+    cat <<END_USAGE
+format.sh - format Datadog C++ source code
+
+usage:
+
+    $argv0 FILES ...
+        Format the specified files in place.
+
+    $argv0
+        Format in place all Datadog-owned C++ code in this repository.
+
+    $argv0 -h
+    $argv0 --help
+        Print this message.
+END_USAGE
+}
+
+if [ $# -eq 1 ] && [ "$1" = '-h' -o "$1" = '--help' ]; then
+    usage
+    exit
+fi
+
+formatter="$(dirname "$0")"/clang-format-6.0.sh
+
+if [ $# -eq 0 ]; then
+    cd "$(git rev-parse --show-toplevel)"
+    find src/ include/ test/ examples/ \
+        -type f \( -name '*.h' -o -name '*.cpp' \) \
+    | xargs "$formatter" -i -style=file
+else
+    exec "$formatter" -i -style=file "$@"
+fi
