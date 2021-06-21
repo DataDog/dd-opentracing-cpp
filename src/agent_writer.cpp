647d5279531e11650740effcfb5ca136c4ceb9dd
David Goffredo <david.goffredo@datadoghq.com>
2021-06-21 16:32:07 -0400

    clang-format-9

--- src/agent_writer.cpp
+++ src/agent_writer.cpp
@@ -26,7 +26,7 @@ AgentWriter::AgentWriter(std::string host, uint32_t port, std::string url,
                          std::chrono::milliseconds write_period,
                          std::shared_ptr<RulesSampler> sampler)
     : AgentWriter(std::unique_ptr<Handle>{new CurlHandle{}}, write_period, max_queued_traces,
-                  default_retry_periods, host, port, url, sampler){}
+                  default_retry_periods, host, port, url, sampler) {}
 
 AgentWriter::AgentWriter(std::unique_ptr<Handle> handle, std::chrono::milliseconds write_period,
                          size_t max_queued_traces,
