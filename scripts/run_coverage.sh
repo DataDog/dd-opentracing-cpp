76a798629ece4cd2b7a9bd75cdb27b8ef509fe67
David Goffredo <david.goffredo@datadoghq.com>
2021-10-26 17:04:41 +0000

    address review comments in scripts/run_coverage.sh:
    - Use only one option (--port=PORT) for configuring the HTTP server port.
    - Remove any existing coverage output before generating a report.

--- scripts/run_coverage.sh
+++ scripts/run_coverage.sh
@@ -7,11 +7,9 @@ unit tests, and then generate a report of the source coverage of the tests.
 
 Options:
 
-  --serve
   --port=<PORT>
-    After generating the coverage report, run an HTTP server on the optionally
-    specified <PORT> that serves the report as HTML.  If <PORT> is not
-    specified (i.e. using the `--serve` option), then it defaults to port 8000.
+    After generating the coverage report, run an HTTP server on the specified
+    <PORT> that serves the report as HTML.
 
   --help
   -h
@@ -22,6 +20,12 @@ END_USAGE
 # Exit if any non-conditional command returns a nonzero exit status.
 set -e
 
+is_integer() {
+  # Any integer is equal to itself, but it's an error if it's not an integer.
+  # Ignore the error message and just use the status code.
+  [ "$1" -eq "$1" ] 2>/dev/null
+}
+
 # If `serve_port` is nonzero, then at the end of this script run an HTTP
 # server, listening on the specified port, serving the coverage report.
 serve_port=0
@@ -29,13 +33,19 @@ serve_port=0
 # Parse command line options.
 while [ $# -gt 0 ]; do
   case "$1" in
-    --help|-h) usage
+    --help|-h)
+      usage
       exit ;;
-    --serve) serve_port=8000 ;;
-    --port=*) serve_port=${1#--port=} ;;
-    *) >&2 usage
+    --port=*)
+      serve_port=${1#--port=}
+      if ! is_integer "$serve_port" || [ "$serve_port" -le 0 ]; then
+        >&2 printf "Invalid port number: %s\n" "$serve_port"
+        exit 1
+      fi ;;
+    *)
+      >&2 usage
       >&2 printf "\nUnknown option: %s\n" "$1"
-      exit 1
+      exit 2
   esac
   shift
 done
@@ -53,6 +63,7 @@ scripts/run_unit_tests.sh --coverage
 
 # Generate a coverage report, remove irrelevant source files from it, and then
 # render the report as a static website.
+rm -rf coverage
 mkdir -p coverage
 lcov --capture --directory "$BUILD_DIR" --output-file coverage/raw.info
 lcov --remove coverage/raw.info -o coverage/filtered.info '/usr/include/*' "$(pwd)/3rd_party/*" "$(pwd)/deps/*" "$(pwd)/test/*"
