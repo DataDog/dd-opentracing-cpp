8562bb97e6ff7c8e3bcea85036738c493e731284
cahillsf <stephen.cahill@datadoghq.com>
2022-02-23 10:06:36 -0500

    adding infinite loop to continually send traces

--- examples/cpp-tracing/compiled-in/tracer_example.cpp
+++ examples/cpp-tracing/compiled-in/tracer_example.cpp
@@ -1,6 +1,6 @@
 #include <datadog/opentracing.h>
 #include <datadog/tags.h>
-
+#include <unistd.h>
 #include <iostream>
 #include <string>
 
@@ -9,14 +9,16 @@ int main(int argc, char* argv[]) {
   auto tracer = datadog::opentracing::makeTracer(tracer_options);
 
   // Create some spans.
-  {
-    auto span_a = tracer->StartSpan("A");
-    span_a->SetTag(datadog::tags::environment, "production");
-    span_a->SetTag("tag", 123);
-    auto span_b = tracer->StartSpan("B", {opentracing::ChildOf(&span_a->context())});
-    span_b->SetTag("tag", "value");
+  while(true){
+    {
+      auto span_a = tracer->StartSpan("A");
+      span_a->SetTag(datadog::tags::environment, "production");
+      span_a->SetTag("tag", 123);
+      auto span_b = tracer->StartSpan("B", {opentracing::ChildOf(&span_a->context())});
+      span_b->SetTag("tag", "value");
+    }
+    tracer->Close();
+    sleep(30);
   }
-
-  tracer->Close();
   return 0;
 }
