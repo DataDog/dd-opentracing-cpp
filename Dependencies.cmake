b0ce19411aed86c7f5264325183292ce4cc13050
Kevin Sucre <me@maztheman.ca>
2023-08-28 10:21:03 -0600

    use cpm, other minor fixes

---
+++ Dependencies.cmake
@@ -0,0 +1,31 @@
+include(cmake/CPM.cmake)
+set(CPM_USE_LOCAL_PACKAGES ON)
+
+function(add_external_dependencies)
+
+CPMAddPackage("gh:catchorg/Catch2@3.3.2")
+CPMAddPackage("gh:CLIUtils/CLI11@2.3.2")
+CPMAddPackage(
+  NAME opentelemetry-cpp
+  GITHUB_REPOSITORY maztheman/opentelemetry-cpp
+  GIT_TAG main
+  OPTIONS
+  "WITH_STL ON" "WITH_OPENTRACING ON" "BUILD_TESTING OFF" "BUILD_STATIC_LIBS ON"
+)
+CPMAddPackage("gh:curl/curl#curl-8_2_1")
+CPMAddPackage(
+  NAME msgpack-c 
+  GITHUB_REPOSITORY msgpack/msgpack-c
+  GIT_TAG cpp-6.1.0
+  OPTIONS "MSGPACK_CXX20 ON"
+)
+CPMAddPackage(
+  NAME nlohmann_json
+  VERSION 3.11.2
+  GITHUB_REPOSITORY nlohmann/json
+  GITHUB_TAG v3.11.2
+  OPTIONS
+    "JSON_BuildTests OFF"
+)
+
+endfunction()
